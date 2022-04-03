5b120cd76b8d35a8d7aaf0fc8fe13642db0a3ec1
JeJo <email2jejo@gmail.com>
2022-04-03 08:20:23 +0200

    FileName - Basic file name class

--- 4_FileService/FileName.cc
+++ 4_FileService/FileName.cc
@@ -1,1 +1,30 @@
 #include "FileName.hpp"
+
+
+
+FileName::FileName(std::filesystem::directory_entry dir, std::string startStr, std::string endStr)
+    : mPath{ std::move(dir) }
+    , mStartWith { std::move(startStr) }
+    , mEndsWith{ std::move(endStr) }
+{}
+
+bool FileName::isCreatedWithIn(std::chrono::duration<double> const& duration) const noexcept
+{
+    return false;
+}
+
+bool FileName::isGoodFile() const
+{
+    if (!mPath.is_regular_file())
+        return false;
+
+    const std::string fileNameStr = this->fileName().string();
+
+    return fileNameStr.starts_with(mStartWith) || fileNameStr.ends_with(mEndsWith) || hasHexadecimal(fileNameStr);
+}
+
+bool FileName::hasHexadecimal(const std::string& str) const
+{
+    return false;
+}
+
--- 4_FileService/FileName.hpp
+++ 4_FileService/FileName.hpp
@@ -1,5 +1,43 @@
 #ifndef FILE_NAME_HPP
 #define FILE_NAME_HPP
 
+#include <string>
+#include <chrono>
+#include <filesystem>
+#include <tuple>
+
+using namespace std::string_literals;
+
+class FileName
+{
+    using PathType = std::filesystem::path;
+    using DirType = std::filesystem::directory_entry;
+public:
+    explicit FileName(std::filesystem::directory_entry dir,
+        std::string startStr = "core"s, std::string endStr = "lz4"s);
+
+    // @TODO Special member functions
+
+    bool isCreatedWithIn(std::chrono::duration<double> const& sec) const noexcept;
+    bool isGoodFile() const;
+
+    const DirType& dir() const { return mPath; }
+
+    PathType file() const { return mPath.path().filename(); }
+
+    PathType fileName() const { return file().stem(); }
+
+    PathType ext() const { return mPath.path().extension(); }
+
+private:
+    bool hasHexadecimal(const std::string& str) const;
+
+
+private:
+    DirType mPath{};
+    std::string mStartWith{};
+    std::string mEndsWith{};
+};
+
 
 #endif // FILE_NAME_HPP
